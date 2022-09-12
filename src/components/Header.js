b956eeb483b795aa259c7e69c11d8142c66a2e01
maikelf <maikelf@gmail.com>
2022-09-12 16:02:17 -0500

    add help and language

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
+import React, { useState } from 'react';
 import logo from '../logo.svg';
 import './Header.css';
 import MenuIcon from '@mui/icons-material/Menu';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export const Header = props => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
     return(
         <header>
             <div className="menu-icon">
@@ -10,6 +18,27 @@ export const Header = props => {
             </div>
             <div className='menu-logo'>
                 <img src={logo} className="logo" alt="logo" />
+            </div>
+            <div className='menu-help'>
+                <HelpOutlineIcon onClick={handleClick} />
+
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    disableScrollLock={true}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <a href="https://www.veterans.gc.ca/eng/financial-support/medical-costs/treatment-benefits/poc_search">
+                            VAC treatment benefits
+                        </a>
+                    </MenuItem>
+                </Menu>
+
+                <div className='menu-lang'>
+                    Français   
+                </div>
             </div>
         </header>
     )
